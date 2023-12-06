9abfb45e6878f5f572eac88fec00341fba17b331
Gento91 <leo.kontente@gmail.com>
2023-12-06 12:00:53 +0100

    potion-fix

--- src/Model/Potion.java
+++ src/Model/Potion.java
@@ -1,2 +1,17 @@
-package src.Model;public class Potion {
+package src.Model;
+
+public class Potion extends Objet{
+    private int HDAmount;
+    public Potion(String nom, int rarete, int HDAmount) {
+        super(nom, rarete);
+        this.HDAmount = HDAmount;
+    }
+
+    public int getHDAmount() {
+        return HDAmount;
+    }
+
+    public void setHDAmount(int HDAmount) {
+        HDAmount = HDAmount;
+    }
 }
